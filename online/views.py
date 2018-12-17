4862db87e2b3ce4351498c8c8a50fdb559d4ab1f
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-17 12:10:05 +0800

    17Django 会话Cookie:1.数据库配置settings.py;2.路由配置urls.py:regist,login,index,logout;3.online:1）view:regist；2） models:User类；3）templates/regist

--- online/views.py
+++ online/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+class UserForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+
+def regist(req):
+	if req.method == "POST":
+		uf = UserForm(req.POST)
+		if uf.is_valid():
+			username = uf.cleaned_data['username']
+			password = uf.cleaned_data['password']
+			print username,password
+			return HttpResponse('ok')
+	else:
+		uf = UserForm
+	return	render_to_response('regist.html',{'uf':uf})
+
--- urls.py
+++ urls.py
@@ -14,4 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'regist/$','online.views.regist'),
+    url(r'login/$','online.views.login'),
+    url(r'index/$','online.views.index'),
+    url(r'logout/$','online.views.logout'),
 )
